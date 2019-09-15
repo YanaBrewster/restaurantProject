2e9593ed1b8be56547a23768dfdbe22fd043062f
Your Name <yana.brewster@gmail.com>
2019-09-15 16:59:44 +1200

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showNav(){
+    var menu = document.getElementById("list");
+    if(menu.style.display === 'block'){
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
